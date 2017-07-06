f0e462a7d3b3fac9f195b4826975b56d58df0ad3
Davide Bontempelli <davide.bonte@gmail.com>
2017-07-06 15:05:14 +0200

    use infix

--- src/FizzBuzz.kt
+++ src/FizzBuzz.kt
@@ -1,12 +1,12 @@
 class FizzBuzz {
     fun evaluate(number: Int): String = when {
-        number.isMultipleOf(3) && number.isMultipleOf(5) -> "FizzBuzz"
-        number.isMultipleOf(5) -> "Buzz"
-        number.isMultipleOf(3) -> "Fizz"
+        number isMultipleOf 3 && number isMultipleOf 5 -> "FizzBuzz"
+        number isMultipleOf 5 -> "Buzz"
+        number isMultipleOf 3 -> "Fizz"
         else -> number.toString()
     }
 
-    fun Int.isMultipleOf(number: Int): Boolean = this % number == 0
+    infix fun Int.isMultipleOf(number: Int): Boolean = this % number == 0
 }
 
 fun main(args: Array<String>) {
